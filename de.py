b05dc512d8e701eb0d07998bb4c4011e9f22a79e
Antonella M Giusiano <antogiusiano31@gmail.com>
2022-07-30 20:04:02 -0300

    Creacion de clase Tablero

--- de.py
+++ de.py
@@ -1,1 +1,28 @@
-print('Hola')
+class Tablero(object):
+
+    def __init__(self, ancho: int, alto: int):
+        self.ancho = ancho
+        self.alto = alto
+        self._tablero = None
+
+    @property
+    def tablero(self):
+        if not self._tablero:
+            self._tablero = [[True for column in range(self.alto)] for row in range(self.ancho)]
+        return self._tablero
+    
+    def dibujar_bloque(self, x: int, y: int):
+        if x < 0 or y <0:
+            raise IndexError(f"La combinacion de x,y:{x},{y} esta fuera de rango.")
+        self.tablero[x][y] = False
+    
+    def obtener_celda(self, x: int, y: int):
+        if x < 0 or y <0:
+            raise IndexError(f"La combinacion de x,y:{x},{y} esta fuera de rango.")
+        return self.tablero[x][y]
+
+    def hay_celda_disponible(self, x: int, y: int):
+            return 0<=x<self.ancho and 0<=y<self.alto and self.tablero[x][y]
+        
+    def copiar(self):
+        return Tablero(self.ancho, self.alto)

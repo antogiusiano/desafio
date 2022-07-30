6e89e68bbfd03b6a25597c550e3bf5197e680fcf
Antonella M Giusiano <antogiusiano31@gmail.com>
2022-07-30 20:24:15 -0300

    Creacion clase Serpiente

--- de.py
+++ de.py
@@ -26,3 +26,44 @@ class Tablero(object):
         
     def copiar(self):
         return Tablero(self.ancho, self.alto)
+
+
+class Serpiente(object):
+    movimientos = {"R": [1, 0], "D": [0, 1], "L": [-1, 0], "U": [0, -1]}
+
+    def __init__(self, serpiente: list):
+        self.serpiente = serpiente
+
+    def det(self, tablero: Tablero):
+        nuevo_tablero = self.celdas_disponibles(tablero)
+        movimientos = self.posibles_movimientos()
+        det = []
+        for m in movimientos:
+            try:
+                if nuevo_tablero.obtener_celda(*m[1]):
+                    det.append(m[0])
+            except IndexError:
+                pass
+        return det
+
+    def posibles_movimientos(self):
+        cabeza = self.serpiente[0]
+        return [ (k, [cabeza[0]+v[0], cabeza[1] +v[1]]) 
+                    for k,v in Serpiente.movimientos.items()]
+
+
+    def celdas_disponibles(self, tablero: Tablero):
+        nuevo_tablero = tablero.copiar()
+        for i in range(len(self.serpiente)-1):
+            body = self.serpiente[i]
+            nuevo_tablero.dibujar_bloque(*body)
+        return nuevo_tablero
+    
+    def movimiento_serpiente(self, direccion: str, tablero: Tablero):
+        if direccion not in Serpiente.movimientos.keys():
+            raise Exception(f"Movimiento \"{direccion}\" desconocido.")
+        if direccion not in self.det(tablero):
+            raise Exception(f"No puede moverse \"{direccion}\" hay algo que te bloquea")
+        cabeza = self.serpiente[0]
+        mover = Serpiente.movimientos[direccion]
+        self.serpiente = [[cabeza[0] + mover[0], cabeza[1] + mover[1]]] + self.serpiente[:-1]
